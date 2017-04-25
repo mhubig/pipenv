e3a4ee3ecfe538a93f1a1bed300492d54f5bc3ca
Nate Prewitt <Nate.Prewitt@gmail.com>
2017-04-25 17:41:19 -0600

    emojis or click?

--- pipenv/cli.py
+++ pipenv/cli.py
@@ -721,7 +721,8 @@ def format_pip_output(out, r=None):
 
 def easter_egg(package_name):
     if package_name in ['requests', 'maya', 'crayons', 'delegator.py', 'records', 'tablib']:
-        click.echo(u'P.S. You have excellent taste! ✨ 🍰 ✨')
+        print(u'P.S. You have excellent taste! ✨ 🍰 ✨')
+        #click.echo(u'P.S. You have excellent taste! ✨ 🍰 ✨')
 
 
 @click.group(invoke_without_command=True)
