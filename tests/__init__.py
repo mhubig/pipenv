94024b672329b3c19ab74fb0c322f6e44fac8994
Nate Prewitt <Nate.Prewitt@gmail.com>
2017-04-26 12:02:39 -0600

    correct vendor path

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,8 @@ import os
 import sys
 
 # Inject vendored directory into system path.
-v_path = os.path.sep.join([os.path.dirname(os.path.realpath(__file__)), 'vendor'])
+
+tests_path = os.sep.join(os.path.dirname(os.path.realpath(__file__)).split(os.sep)[:-1])
+v_path = os.path.sep.join([tests_path, 'pipenv', 'vendor'])
+
 sys.path.append(v_path)
